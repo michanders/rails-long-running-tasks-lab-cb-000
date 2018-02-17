b2e5dfddf83ade6c47a8bce832de4c2bc2ee033d
Michael Anderson <michanders@msn.com>
2018-02-17 22:45:23 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs, only: [:index]
-  post 'songs/upload', to 'songs#upload'
+  post 'songs/upload', to: 'songs#upload'
 
 end
